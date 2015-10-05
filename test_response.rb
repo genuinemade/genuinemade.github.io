90e9c42f018d18e94831c9538468aa7cf0a8aef0
donald <donald199209@gmail.com>
2015-10-05 23:42:40 +0800

    update

--- test_response.rb
+++ test_response.rb
@@ -1,34 +1,70 @@
 require 'net/http'
+require 'nokogiri'
+require 'thread'
+require 'ruby-prof'
 
-def test_url(url)
-  uri = URI.parse(URI.encode(url))
-  res = Net::HTTP.get_response(uri)
-
-  if res.code == "200"
-    true
-  else
-    false
+def get_response(url)
+  begin
+    uri = URI.parse(URI.encode(url.sub(/#\w+/, ""))) 
+    Net::HTTP.get_response(uri)
+  rescue Exception => e
+    puts url
+    puts "error"
   end
 end
 
-def test_site(host, paths)
-  paths.map do |path|
-    url = File.join(host, path) + ".html"
-    res = test_url(url)
-    if res == true
-      puts "#{url} \033[0;32mGOOD\033[0m"
-    else
-      puts "#{url} \033[0;31mBAD\033[0m"
-    end
-    res
+def test_path(root, path, depth = 0)
+  url = File.join(root, path)
+  res = get_response(url)
+  deal_res(url, res)
+  test_links(root, res.body) if depth == 0
+end
+
+def deal_res(url, res)
+  if res.code == "200"
+    puts "#{url} \033[0;32mGOOD\033[0m"
+  else
+    puts "#{url} \033[0;31mBAD\033[0m"
   end
+end
+
+def test_site(root, paths)
+  thr = paths.map do |path|
+    Thread.new{test_path(root, path + ".html")}
+  end
+  thr.each(&:join)
+end
+
+def test_links(root, body)
+  Nokogiri::HTML(body).css("link").each do |no|
+    test_path(root, no["href"], 1)
+  end
+  Nokogiri::HTML(body).css("img").each do |no|
+    test_path(root, no["src"], 1)
+  end
+  Nokogiri::HTML(body).css("script").each do |no|
+    test_path(root, no["src"], 1) if no["src"]
+  end
+  # Nokogiri::HTML(body).css("a").each do |no|
+  #   if no["href"] and !(/javascript/ === no["href"]) and !(/mailto/ === no["href"])
+  #     h = no["href"]
+  #     if /^(https?)/ === h
+  #       test_path(h, "", 1)
+  #     else
+  #       test_path(root, h, 1)  
+  #     end
+  #   end
+  # end
 end
 
 paths = %w[index gadget iphone pc print tpu]
-host = "http://www.genuinemade.com"
+root = "http://www.genuinemade.com"
+puts "start ..."
+test_site(root, paths)
+puts "end ..."
 
-if test_site(host, paths).any?{|r| r == false}
-  puts "\033[0;31m有地址访问异常 !!\033[0m"
-else
-  puts "OK !!"
-end
+paths = %w[pc tpu tpu_edge]
+root = "http://www.genuinemade.com/s6"
+puts "start ..."
+test_site(root, paths)
+puts "end ..."
